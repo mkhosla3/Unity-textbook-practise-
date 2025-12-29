fcf2847a18a0ec0278fbbb9ed5cce164978c517b
mkhosla3 <mehrkhosla5@gmail.com>
2025-12-29 23:35:25 +0530

    added till chapter 5

---
+++ Practising Unity/Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("test Start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Test");
+        
+    }
+}
